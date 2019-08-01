5a51efe93e3e6519404fc6643b17151cbbd319c7
Alex <PR1ME-GOP@yandex.ru>
2019-08-01 21:13:58 +0400

    Стартовый шаблон

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.scss';
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.setState = {
+
+		};
+	}
+
+	render() {
+
+		return (
+			<div className="todo-app">
+				<p>Start React</p>
+			</div>
+		);
+	}
+};
+
+export default App;
